a22359a35ddb8dc3a2db16f1430920da533616b1
Mehfooz-ur-Rehman <mehfoozijaz786@gmail.com>
2021-09-26 20:31:30 +0500

    initial code

---
+++ api/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+//api config
+const app = express();
+const port = process.env.PORT || 8800;
+
+//middlewares
+
+//db config
+
+//api endpoints
+
+//listners
+app.listen(port, () => {
+  console.log("server running");
+});
